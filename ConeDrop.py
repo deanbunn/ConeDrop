67fd0516c4639eaecddf035dbe73c88ac15ee69f
ucdncode <ncode@ucdavis.edu>
2022-11-02 10:04:35 -0700

    20221102_1004hrs Commented Code

--- ConeDrop.py
+++ ConeDrop.py
@@ -1,12 +1,19 @@
 import sys
 
 def drop_cone(cone_numb):
+    #Var for Cone Comparison
     x = 0
+
+    #Print Blank Line
+    print("\n")
+
+    #While Loop to Print Cones
     while x < cone_numb:
        print('\u0394')
        x += 1
 
-       
+
+    #Var for Message to User About Cones Dropped   
     drop_msg = "You dropped " + str(cone_numb)
 
     if(cone_numb > 1):
@@ -15,6 +22,7 @@ def drop_cone(cone_numb):
        drop_msg += " cone"
     
     
+    #Notify User How Many Cones Dropped
     print("\n" + drop_msg + "\n")
 
 
@@ -25,12 +33,16 @@ usr_choice = ""
 #Var for Checking to Continue
 b_cont = True
 
+#While Loop to Keep the Cone Drop Party Going
 while b_cont == True:   
+   #Ask User for Cone Action
    usr_choice = input("Would you like to drop some cones? Y=Yes,N=No: ")
    
+   #Check Answer for Yes and Number of Cones
    if(usr_choice.lower() == "y"):
       usr_cone_numb = input("\nHow many cones would you like to drop? ")
       
+      #Quick Number Check on Cone Number Input
       if(usr_cone_numb.isdigit()):
           drop_cone(int(usr_cone_numb))
       else:
@@ -38,6 +50,7 @@ while b_cont == True:
  
       
    else:
+      #User Doesn't Want to Continue. Stop Loop
       b_cont = False
 
 
